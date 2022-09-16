797d603d792dae8d390cd3e3b0e4e580648a7932
THeMuCiT127 <themucit127@gmail.com>
2022-09-16 12:52:18 +0300

    first commit

---
+++ frontend/src/pages/main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Animation from '../../components/animation/Animation'
+
+import './main.css'
+
+const Main = () => {
+     return (
+          <div className='main'>
+               <Animation />
+          </div>
+     )
+}
+
+export default Main
